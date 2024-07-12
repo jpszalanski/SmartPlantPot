92630692c2faae4421a9e6a0df2c31e622f0b70c
Jeferson Szalanski <jefersonszalanski@gmail.com>
2024-07-12 19:21:14 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,10 @@ PubSubClient client(espClient);
 DHT dht(DHTPIN, DHTTYPE);
 
 const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 0;
+const long gmtOffset_sec = -10800; // Offset de -3 horas em segundos
 const int daylightOffset_sec = 3600;
 
-// configurar o WiFi
+// Configurar o WiFi
 void setup_wifi()
 {
   delay(10);
@@ -51,13 +51,33 @@ void callback(char *topic, byte *payload, unsigned int length)
   Serial.print("Mensagem recebida no tópico: ");
   Serial.print(topic);
   Serial.print(". Mensagem: ");
+  String message;
   for (int i = 0; i < length; i++)
   {
     Serial.print((char)payload[i]);
+    message += (char)payload[i];
   }
   Serial.println();
+
+  // Verifica se a mensagem recebida é para acionar a bomba de água
+  if (message == "water_on")
+  {
+    // water_on();
+  }
+  else if (message == "water_off")
+  {
+    // water_off();
+  }
+}
+void water_on()
+{
+  digitalWrite(WATER_PUMP_PIN, LOW);
 }
 
+void water_off()
+{
+  digitalWrite(WATER_PUMP_PIN, HIGH);
+}
 void reconnect()
 {
   String clientId = "ESP32Client-" + String(WiFi.macAddress());
@@ -66,13 +86,14 @@ void reconnect()
     Serial.print("Tentando conectar ao MQTT...");
     if (client.connect(clientId.c_str()))
     {
-      Serial.println("conectado");
-      client.subscribe(AWS_SUB_TOPIC_ACCEPTED);
-      client.subscribe(AWS_SUB_TOPIC_REJECTED);
+      Serial.println("Conectado");
+      client.subscribe("$aws/things/SmartPlantPot/shadow/update/accepted");
+      client.subscribe("$aws/things/SmartPlantPot/shadow/update/rejected");
+      client.subscribe("$aws/things/SmartPlantPot/control"); // tópico de controle
     }
     else
     {
-      Serial.print("falhou, rc=");
+      Serial.print("Falhou, rc=");
       Serial.print(client.state());
       Serial.println(" tentando novamente em 5 segundos");
       delay(5000);
@@ -85,7 +106,7 @@ void setup_pins()
   pinMode(LDRPIN, INPUT);
   pinMode(SOIL_MOISTURE_PIN, INPUT);
   pinMode(WATER_PUMP_PIN, OUTPUT);
-  digitalWrite(WATER_PUMP_PIN, HIGH); // iniciar desligado
+  digitalWrite(WATER_PUMP_PIN, HIGH); // Iniciar desligado
 }
 
 void setup_aws()
@@ -104,18 +125,39 @@ void setup_time()
 
 String getTimestamp()
 {
-  time_t now;
   struct tm timeinfo;
+  char timestamp[30];
+  char msec[5];
+
   if (!getLocalTime(&timeinfo))
   {
     Serial.println("Falha ao obter o tempo");
     return "";
   }
-  time(&now);
-  char timestamp[20];
-  strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
+
+  // Obtém o tempo atual com milissegundos
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+
+  // Formata o tempo em segundos
+  strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", &timeinfo);
+
+  // Formata os milissegundos
+  snprintf(msec, sizeof(msec), ".%03d", tv.tv_usec / 1000);
+
+  // Concatena os milissegundos ao timestamp
+  strcat(timestamp, msec);
+
+  // Adiciona o sufixo "Z"
+  strcat(timestamp, "Z");
+
+  // Debug adicional
+  Serial.print("Timestamp gerado: ");
+  Serial.println(timestamp);
+
   return String(timestamp);
 }
+
 void setup()
 {
   Serial.begin(115200);
@@ -124,48 +166,33 @@ void setup()
   setup_wifi();
   setup_aws();
   setup_time();
-  // client.setServer(AWS_IOT_ENDPOINT, AWS_PORT);
   reconnect();
 }
 
-void publishAws()
+void updateShadow()
 {
   String timestamp = getTimestamp();
+
   String payload = "{\"state\":{\"reported\":{";
   payload += "\"deviceId\": \"" + String(WiFi.macAddress()) + "\",";
-  payload += "\"createdAt\": \"" + timestamp + "\",";
-  payload += "\"updatedAt\": \"" + timestamp + "\",";
   payload += "\"humidity\": " + String(humidity) + ",";
-  payload += "\"temperature\": " + String(temperature) + ",";
   payload += "\"light\": " + String(lightLevel) + ",";
   payload += "\"soilMoisture\": " + String(soilMoisture) + ",";
+  payload += "\"temperature\": " + String(temperature) + ",";
   payload += "\"timestamp\": \"" + timestamp + "\"}}}";
 
-  Serial.print("Enviando payload para o tópico ");
-  Serial.print(AWS_PUB_TOPIC);
-  Serial.print(": ");
+  Serial.print("Atualizando Shadow com payload: ");
   Serial.println(payload);
-  Serial.println("---------------------------------");
 
-  if (client.publish(AWS_PUB_TOPIC, payload.c_str()))
+  if (client.publish("$aws/things/SmartPlantPot/shadow/update", payload.c_str()))
   {
-    Serial.println("Mensagem publicada com sucesso");
+    Serial.println("Shadow atualizado com sucesso");
     last_pub_aws = millis();
   }
   else
   {
-    Serial.println("Falha ao publicar mensagem");
+    Serial.println("Falha ao atualizar o Shadow");
   }
-}
-
-void water_on()
-{
-  digitalWrite(WATER_PUMP_PIN, LOW);
-}
-
-void water_off()
-{
-  digitalWrite(WATER_PUMP_PIN, HIGH);
 }
 
 void water_pump()
@@ -230,7 +257,7 @@ void loop()
 
   if (millis() - last_pub_aws >= interval_pub || last_pub_aws == 0)
   {
-    publishAws();
+    updateShadow();
   }
   delay(5000);
 }
